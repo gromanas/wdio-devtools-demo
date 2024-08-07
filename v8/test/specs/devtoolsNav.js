96310a9fdc3df19ee4c772c8b3941de38311427a
nikos-lytras <nikolaos.lytras@allwyn-lotterysolutions.com>
2024-08-07 11:48:13 +0300

    [NO-JIRA]: clean up v8

--- v8/test/specs/devtoolsNav.js
+++ v8/test/specs/devtoolsNav.js
@@ -2,20 +2,33 @@ import {$, browser} from "@wdio/globals";
 
 describe("DevTools Test on latest chrome", function() {
     it("Devtools Navigation", async function() {
+        // Open an example game.
         await browser.url("https://pixijs.io/examples-v5/#/demos-basic/container.js");
-        await browser.pause(2000);
+
+        // Get window handles.
+        // Handle on index 0 is the main window.
+        // Handle on index 1 is the devtools window.
         const handles = await browser.getWindowHandles();
+
+        // Switch to devtools window.
         await browser.switchToWindow(handles[1]);
-        await browser.pause(1000);
+
+        // Navigate through devtool tabs.
+        // For linux machines use ['Control', '['] .
         await browser.keys(['Meta', '[']);
+
+        // Switch to devtool extension iframe.
         await browser.switchToFrame(0);
-        await browser.pause(1000);
+
+        // Manipulate extension.
         await $("body > div > div > div.status.svelte-iqeeoq > div.patch.svelte-iqeeoq > button").click();
         await $("body > div > div > div.outliner.svelte-iqeeoq > div > div.body.svelte-1vjyr8f > div:nth-child(1) > button:nth-child(4)").click();
-        await browser.pause(1000);
         await $("body > div > div > div.outliner.svelte-iqeeoq > div > div.body.svelte-1vjyr8f > div:nth-child(1) > button:nth-child(4)").click();
+
+        // Switch back to parent frame in case that you want to continue the navigation through devtool tabs.
         await browser.switchToParentFrame();
+
+        // Switch back to main window.
         await browser.switchToWindow(handles[0]);
-        await browser.pause(10000);
     });
 });
